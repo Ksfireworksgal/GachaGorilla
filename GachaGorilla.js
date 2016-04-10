0419ff667c3a2d79383fb3d5b885ae3b06d70cb5
Lynette White <runethesweet@gmail.com>
2016-04-10 18:01:56 -0500

    OMG i hate naming errors

--- GachaGorilla.js
+++ GachaGorilla.js
@@ -35,5 +35,37 @@ Template.gachaItem.events({
     gachaList.remove(this._id);
   }
 });
+Template.gachaItem.helpers({
+  editing: function(){
+    return Session.equals('editItemId', this._id);
+  } 
+});
+ 
+Template.gachaItem.events({
+  'click .deleteItem': function(){
+    Items.remove(this._id);
+  },
+  'click .editItem': function(){
+    Session.set('editItemId', this._id);
+  }
+});
+
+
+var saveItem = function(){
+  var editItem = {
+      gachaDesigner: $('#gachaDesigner').val(),
+      gachaEvent: $('#gachaEvent').val(),
+      gachaName: $('#gachaName').val(),
+      gachaColor: $('#gachaColor').val(),
+      gachaSize: $('#gachaSize').val(),
+      gachaRarity: $('#gachaRarity').val(),
+      gachaQty: $('#gachaQty').val(),
+      gachaPrice: $('#gachaPrice').val(),
+  }
+ 
+  Items.update(Session.get('editItemId'), {$set: editItem});
+  Session.set('editItemId', null);
+
+}
 
 }
