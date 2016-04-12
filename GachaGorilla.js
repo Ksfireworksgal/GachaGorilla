24f737bdaa262456a3e6ecbd40bbdbb68ffcd605
Lynette White <runethesweet@gmail.com>
2016-04-11 20:47:34 -0500

    cont to work on structuring for input

--- GachaGorilla.js
+++ GachaGorilla.js
@@ -29,6 +29,67 @@ Template.item.events({
   }
 });
 
+Template.item.helpers({
+  editing: function(){
+    return Session.equals('editItemId', this._id);
+  } 
+});
+
+Template.item.events({
+  'click .deleteItem': function(){
+    Items.remove(this._id);
+  },
+  'click .editItem': function(){
+    Session.set('editItemId', this._id);
+  }
+});
+
+
+  'click .editItem': function(){
+    Session.set('editItemId', this._id);
+  },
+  'click .cancelItem': function(){
+    Session.set('editItemId', null);
+  },
+
+var saveItem = function(){
+  var editItem = {
+    designer: $("#editItemdesigner").val(),
+    eventName: $("#editItemeventName").val(),
+    gachaName: $("#editItemgachaName").val(),
+    rarity: $("#editItemrairty").val(),
+    color: $("#editItemcolor").val(),
+    size: $("#editItemsize").val(),
+    qty: $("#editItemqty").val()
+    price: $("#editItemPrice").val()
+  }
+
+  Items.update(Session.get('editItemId'), {$set: editItem});
+  Session.set('editItemId', null);
+}
+
+  'click .cancelItem': function(){
+    Session.set('editItemId', null);
+  },
+  'click .saveItem': function(){
+    saveItem();
+  }
+
+
+
+  'click .saveItem': function(){
+    saveItem();
+  },
+  'keypress input': function(e){
+    if(e.keyCode === 13){
+      saveItem();
+    }
+    else if(e.keyCode === 27){
+      Session.set('editItemId', null);
+    }
+  }
+
+
 
 
 
